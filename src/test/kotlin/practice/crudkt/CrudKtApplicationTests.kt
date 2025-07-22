68953e65c59e969ae81a4b7c1118240703e5d13e
seungchan <s24041@gsm.hs.kr>
2025-07-22 22:22:50 +0900

    init

---
+++ src/test/kotlin/practice/crudkt/CrudKtApplicationTests.kt
@@ -0,0 +1,13 @@
+package practice.crudkt
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CrudKtApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
